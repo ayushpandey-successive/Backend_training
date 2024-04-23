7fb93772150efd205a402590ae57516446321137
Ayush Pandey <ayush.pandey@ayushpandeyr-422.successivetech.com>
2024-04-23 16:50:26 +0530

    Day-2 Assignment

---
+++ Day-2/mockData.js
@@ -0,0 +1,17 @@
+const data = [
+    {
+        name: 'Anant',
+        email : 'anant@gmail.com'
+    },
+    {
+        name: 'Ayush',
+        email : 'ayush@gmail.com'
+    },
+    {
+        name: 'Udit',
+        email : 'udit@gmail.com'
+    }
+]
+
+
+module.exports = data
