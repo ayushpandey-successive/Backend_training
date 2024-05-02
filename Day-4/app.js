78540f4866796fb92781a84b1586e24e5721c34c
Ayush Pandey <ayush.pandey@ayushpandeyr-422.successivetech.com>
2024-05-02 09:34:46 +0530

    4th Assignment

--- Day-4/app.js
+++ Day-4/app.js
@@ -7,15 +7,20 @@ const userRoutes=require("./routes/postsRoutes")
 const authRouter=require("./routes/authenticationRoute")
 const logRequestDetails=require('./middleware/logRequestDetails')
 const { errorHandler } = require('./middleware/errorHandler')
-
+const {customHeaderMiddleware}=require('./middleware/customHeaderMiddleware')
+const {rateLimitMiddleware}= require('./middleware/rateLimitMiddleware')
 
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
 app.use(logRequestDetails)
+app.use(customHeaderMiddleware)
 app.use('/users',userRoutes)
 app.use("/auth",authRouter)
+app.get('/custom',rateLimitMiddleware,(req,res,next)=>{
+  res.send("hello world");
+})
 
 app.get('*',(req,res,next)=>{
   try {
